b3dae7e8ac8f62f5f024ff86e249fffa0f493ef5
riyajath-ahamed <riyajatha@gmail.com>
2025-08-27 19:53:51 +0530

    micro FE  | RJ

--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,14 @@ import federation from "@originjs/vite-plugin-federation";
 
 export default defineConfig({
   server: {
+    host: '0.0.0.0',
     port: 5001,
     cors: true,
+  },
+  preview:{
+    host: '0.0.0.0',
+    port: 5001, // Ensure the preview server runs on the same port
+    cors: true, // Enable CORS for the preview server
   },
   plugins: [
     react(),
